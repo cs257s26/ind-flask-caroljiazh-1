4a4421f391d4a2bb38eaca003966e370300c2446
caroljiazh <zhangc5@carleton.edu>
2026-05-09 15:50:03 -0400

    added code to datasource.py

--- .ipynb_checkpoints/datasource-checkpoint.py
+++ .ipynb_checkpoints/datasource-checkpoint.py
@@ -19,7 +19,7 @@ def connect():
         exit()
     return connection 
 
-def country_avg_schooling():
+def country_avg_schooling(connection, country, start_year, end_year):
     """
     Returns the average years of schooling for a country within a given year range.
     
@@ -31,9 +31,17 @@ def country_avg_schooling():
     Returns:
         list - rows of (country, year, avg_years_schooling) or None if no data
     """
-    pass
+    try:
+        cursor = connection.cursor()
+        query = """SELECT country, year, avg_years_schooling FROM schooling WHERE LOWER(country) = LOWER(%s) AND year >= %s AND year <= %s ORDER BY year ASC;"""
+        cursor.execute(query, (country, start_year, end_year))
+        return cursor.fetchall()
+        
+    except Exception as e:
+        print ("Something went wrong when executing the query: ", e)
+        return None
 
-def top_country_year():
+def top_country_year(connection, year, country_number):
     """
     Returns a given number of top countries with the highest average years of schooling within a given year. 
     
@@ -45,7 +53,18 @@ def top_country_year():
     Return:
         list - a list of the top (input number) countries with the highest average years of schooling in a given year
     """
-    pass
+    try:
+        cursor = connection.cursor()
+        query = """SELECT country, year, avg_years_schooling FROM schooling WHERE year = %s ORDER BY avg_years_schooling DESC LIMIT %s;"""
+        cursor.execute(query, (year, country_number))
+        return cursor.fetchall()
+
+    except Exception as e:
+        print ("Something went wrong when executing the query: ", e)
+        return None
 
 def main():
-    pass
+    connection = connect()
+    print(country_avg_schooling(connection, 'France', 2015, 2020))
+    print(top_country_year(connection, 2020, 5))
+    connection.close()
