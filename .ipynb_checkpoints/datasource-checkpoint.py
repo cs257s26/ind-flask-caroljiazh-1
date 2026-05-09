f067cacd9e38b85f28b28f97533857134a0bdfe1
caroljiazh <zhangc5@carleton.edu>
2026-05-09 15:13:13 -0400

    added 2 query functions' comments to datasource.py

--- .ipynb_checkpoints/datasource-checkpoint.py
+++ .ipynb_checkpoints/datasource-checkpoint.py
@@ -9,7 +9,8 @@ def connect():
     Returns: a database connection.
 
     Note: exits if a connection cannot be established.
-    This function comes from psycopg2-sample.py given from the intro DB lab
+    
+    This function's code and comments come from psycopg2-sample.py given in the intro DB lab on Moodle
     """
     try:
         connection = ps.connect(database=config.database, user=config.user, password=config.password, host="localhost")
@@ -17,3 +18,34 @@ def connect():
         print("Connection error: ", e)
         exit()
     return connection 
+
+def country_avg_schooling():
+    """
+    Returns the average years of schooling for a country within a given year range.
+    
+    Args:
+        connection (psycopg2.connection) - the connection to the database
+        country (str) - the desired country
+        start_year (int) - beginning inclusive year range
+        end_year (int) - ending inclusive year range
+    Returns:
+        list - rows of (country, year, avg_years_schooling) or None if no data
+    """
+    pass
+
+def top_country_year():
+    """
+    Returns a given number of top countries with the highest average years of schooling within a given year. 
+    
+    Args:
+        connection (psycopg2.connection) - the connection to the database
+        year (int) - the designated year
+        country_number (int) - the desired number of countries outputted
+        
+    Return:
+        list - a list of the top (input number) countries with the highest average years of schooling in a given year
+    """
+    pass
+
+def main():
+    pass
